50a914487fec40d99580e1e607fddced2daedc1e
李鑫玮 <>
2021-08-21 17:34:56 +0800

    组测试配置到方法

--- testng/src/main/java/com/guazi/groups/GroupsOnMethod.java
+++ testng/src/main/java/com/guazi/groups/GroupsOnMethod.java
@@ -1,5 +1,9 @@
 package com.guazi.groups;
 
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
 /**
  * @ClassName GroupsOnMethod
  * @Description TODO
@@ -8,6 +12,45 @@ package com.guazi.groups;
  * @Version 1.0
  **/
 public class GroupsOnMethod {
-    public static void main(String[] args) {
+
+    @Test(groups = "server")
+    public void test1() {
+        System.out.println("这是服务端组的测试方法1111111");
+    }
+
+    @Test(groups = "server")
+    public void test2() {
+        System.out.println("这是服务端组的测试方法2222222");
+    }
+
+    @Test(groups = "client")
+    public void test3() {
+        System.out.println("这是客户端组的测试方法3333333");
+    }
+
+    @Test(groups = "client")
+    public void test4() {
+        System.out.println("这是客户端组的测试方法4444444");
+    }
+
+    @BeforeGroups("server")
+    public void beforeGroupsOnServer() {
+        System.out.println("这是服务端组运行前运行的方法");
+    }
+
+    @AfterGroups("server")
+    public void afterGroupsOnServer() {
+        System.out.println("这是服务端组运行后运行的方法");
+        System.out.println("=======================");
+    }
+
+    @BeforeGroups("client")
+    public void beforeGroupsOnClient() {
+        System.out.println("这是客户端组运行前运行的方法");
+    }
+
+    @AfterGroups(value = "client",enabled = false)
+    public void afterGroupsOnClient() {
+        System.out.println("这是客户端组运行后运行的方法");
     }
 }
