7094eb8c1d8075e126a2697aa000618915912062
李鑫玮 <>
2021-08-21 12:08:33 +0800

    增加groups组测试

---
+++ testng/src/main/java/com/guazi/groups/GroupsOnMethod.java
@@ -0,0 +1,13 @@
+package com.guazi.groups;
+
+/**
+ * @ClassName GroupsOnMethod
+ * @Description TODO
+ * @Author lixinwei
+ * @Date 2021/8/21 11:37 上午
+ * @Version 1.0
+ **/
+public class GroupsOnMethod {
+    public static void main(String[] args) {
+    }
+}
