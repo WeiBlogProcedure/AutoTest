c98c95e2cea44233dc0ecdf20f4ce703ad309f86
李鑫玮 <>
2021-08-21 18:53:40 +0800

    参数化测试

---
+++ testng/src/main/java/com/guazi/parameter/ParameterTest.java
@@ -0,0 +1,12 @@
+package com.guazi.paramter;
+
+/**
+ * @ClassName ParamterTest
+ * @Description TODO
+ * @Author lixinwei
+ * @Date 2021/8/21 6:40 下午
+ * @Version 1.0
+ **/
+public class ParamterTest {
+
+}
