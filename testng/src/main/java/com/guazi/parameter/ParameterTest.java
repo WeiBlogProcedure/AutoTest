6538ab896872968f5fd7be54ee049b3f0205df73
李鑫玮 <>
2021-08-21 18:56:29 +0800

    参数化测试-xml文件参数化

--- testng/src/main/java/com/guazi/parameter/ParameterTest.java
+++ testng/src/main/java/com/guazi/parameter/ParameterTest.java
@@ -1,12 +1,20 @@
-package com.guazi.paramter;
+package com.guazi.parameter;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 /**
- * @ClassName ParamterTest
+ * @ClassName ParameterTest
  * @Description TODO
  * @Author lixinwei
  * @Date 2021/8/21 6:40 下午
  * @Version 1.0
  **/
-public class ParamterTest {
+public class ParameterTest {
 
+    @Test
+    @Parameters({"name","age"})
+    public void parameterTest1(String name, int age) {
+        System.out.println("name = " + name + " : age = " + age);
+    }
 }
