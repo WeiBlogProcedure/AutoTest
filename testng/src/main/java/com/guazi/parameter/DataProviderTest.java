0fcb6b06455bd89c304d0e48e5579844818e3c1b
李鑫玮 <>
2021-08-22 11:24:18 +0800

    参数化测试-DataProvider参数化

---
+++ testng/src/main/java/com/guazi/parameter/DataProviderTest.java
@@ -0,0 +1,11 @@
+package com.guazi.parameter;/**
+  * @ClassName DataProviderTest
+  * @Description TODO
+  * @Author lixinwei
+  * @Date 2021/8/22 11:01 上午
+  * @Version 1.0
+  **/
+public class DataProviderTest {
+    public static void main(String[] args) {
+    }
+}
