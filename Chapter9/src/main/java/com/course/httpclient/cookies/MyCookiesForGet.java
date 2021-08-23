4d8f50cfb84e8fb4d0f750a0562a063e0ef44a8d
李鑫玮 <>
2021-08-23 21:59:51 +0800

    Mock一个返回Cookies信息的请求

---
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -0,0 +1,13 @@
+package com.course.httpclient.cookies;
+
+/**
+ * @ClassName MyCookiesForGet
+ * @Description TODO
+ * @Author lixinwei
+ * @Date 2021/8/23 9:59 下午
+ * @Version 1.0
+ **/
+public class MyCookiesForGet {
+    public static void main(String[] args) {
+    }
+}
