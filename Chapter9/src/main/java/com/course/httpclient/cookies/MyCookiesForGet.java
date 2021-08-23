728777b34814faedc73b79ab1e908a1b1c1ba3e2
李鑫玮 <>
2021-08-23 22:27:07 +0800

    获取Cookies信息

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -1,13 +1,16 @@
 package com.course.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -40,5 +43,16 @@ public class MyCookiesForGet {
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
+
+        //获取cookies信息
+        CookieStore store = client.getCookieStore();
+        List<Cookie> cookieList = store.getCookies();
+
+        for (Cookie cookie : cookieList) {
+            String name = cookie.getName();
+            String value = cookie.getValue();
+            System.out.println("cookie-name = " + name +
+                    " : cookie-value = " + value);
+        }
     }
 }
