3f6b96d0c1d4af76879a3c65610e2e6d2e72f206
李鑫玮 <>
2021-08-23 22:43:17 +0800

    携带Cookies信息访问get请求

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -24,7 +24,7 @@ import java.util.ResourceBundle;
 public class MyCookiesForGet {
     private String host;
     private ResourceBundle bundle;//用来读取配置文件
-
+    private CookieStore store;//用来存储cookies信息的变量
     @BeforeTest
     public void beforeTest() {
         bundle = ResourceBundle.getBundle("application", Locale.CHINA);//默认读取resources下的.properties文件
@@ -38,6 +38,7 @@ public class MyCookiesForGet {
         String uri = bundle.getString("getCookies.uri");
         String url = this.host+uri;
 
+        //测试逻辑代码书写
         HttpGet get= new HttpGet(url);
         DefaultHttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
@@ -45,7 +46,7 @@ public class MyCookiesForGet {
         System.out.println(result);
 
         //获取cookies信息
-        CookieStore store = client.getCookieStore();
+        this.store = client.getCookieStore();
         List<Cookie> cookieList = store.getCookies();
 
         for (Cookie cookie : cookieList) {
@@ -53,6 +54,35 @@ public class MyCookiesForGet {
             String value = cookie.getValue();
             System.out.println("cookie-name = " + name +
                     " : cookie-value = " + value);
+        }
+    }
+
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies() throws IOException {
+        String uri = bundle.getString("test.get.with.cookies");
+        String url = this.host+uri;
+
+        HttpGet get = new HttpGet(url);
+        DefaultHttpClient client = new DefaultHttpClient();
+
+        //设置cookies信息
+        client.setCookieStore(this.store);
+
+        HttpResponse response = client.execute(get);
+
+        //获取响应的状态码
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("statusCode = " + statusCode);
+
+        if (statusCode == 200) {
+            String result = EntityUtils.toString(response.getEntity(), "utf-8");
+            System.out.println(result);
+        }else {
+            try {
+                throw new Exception("运行失败");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
